a15ae1b41d677a122716561b2341acd97a52e028
hema-developer <Hema.Chegu@Precisionvh.com>
2025-03-14 19:01:37 +0530

    datatypes

---
+++ Js Code/datatypes/script.js
@@ -0,0 +1,18 @@
+let a=5;
+console.log(typeof a);  //number
+
+let b="Prasad"
+console.log(typeof b);  //string
+
+let c=true;
+console.log(typeof c);  //boolean
+
+//implict type casting
+let d=5+"5";
+console.log(d,typeof d); //55,string
+
+let e="prasad"+2;
+console.log(e, typeof e); //prasad2,string
+
+let f=1+true;
+console.log(f,typeof f); //2,number
